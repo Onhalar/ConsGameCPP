95f3ae1f1160c5fc5cdcefe7a20cbd15fd272672
Onhalar <halaczondrej@gmail.com>
2024-10-20 00:19:03 +0200

    added console and basic movement

--- source/game.cpp
+++ source/game.cpp
@@ -6,14 +6,20 @@
 #include <iostream>
 #include <array>
 #include <format>
+#include <variant>
+#include <map>
+#include <sstream>
+#include <set>
 
 using namespace std;
 
 using Position = array<int, 2>;
+using Vector = Position;
 using Cell = vector<string>;
 using DataLayer = vector<Cell>;
 using Grid = vector<DataLayer>;
 using Size = array<unsigned int, 2>;
+using Event = map<string, variant<char, int>>;
 
 Grid globalGridMap;
 Grid gridMap;
@@ -22,6 +28,21 @@ Size screenSize = {3, 3}; //y, x
 Position position = {3, 3}; //y, x
 Size levelDimensions = {5, 6}; //y, x
 Size TileSize = {3, 3};
+
+//movement translation
+set<string> possibleMovementTranslation = {"up", "down", "left", "right"};
+map<string, char> directionText = {
+    {"up", 'N'},
+    {"down", 'S'},
+    {"left", 'W'},
+    {"right", 'E'}
+};
+map<char, Position> CompassDirection = {
+    {'N', {-1, 0}},
+    {'S', {1, 0}},
+    {'W', {0, -1}},
+    {'E', {0, 1}}
+};
 
 map<string, Cell> systemTiles = {
     {"solid", {"######", "######", "######"}},
@@ -49,6 +70,21 @@ DataLayer tiles = { // {type, fill, span}
     {"solid", "system", "4"}
 };
 
+// *************** Helper functions ***************
+
+Position GetConsoleSize() { //y, x
+    CONSOLE_SCREEN_BUFFER_INFO sbInfo;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbInfo);
+    return {sbInfo.srWindow.Bottom - sbInfo.srWindow.Top, sbInfo.srWindow.Right - sbInfo.srWindow.Left};
+}
+
+bool isInBounds (const Position positionToCheck) {
+    return positionToCheck[0] >= 0 && positionToCheck[0] < (int)gridMap.size() && positionToCheck[1] >= 0 && positionToCheck[1] < (int)gridMap[0].size() ? true : false;
+}
+bool isTraversable (const Position positionToCheck) {
+    return gridMap[positionToCheck[0]][positionToCheck[1]][0] == "empty" ? true : false;
+}
+
 string chainText(const string& str, const int& times) {
     string result;
     for (int i = 0; i < times; ++i) {
@@ -57,11 +93,56 @@ string chainText(const string& str, const int& times) {
     return result;
 }
 
-Position GetConsoleSize() { //y, x
-    CONSOLE_SCREEN_BUFFER_INFO sbInfo;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbInfo);
-    return {sbInfo.srWindow.Bottom - sbInfo.srWindow.Top, sbInfo.srWindow.Right - sbInfo.srWindow.Left};
+void clearLines (const unsigned int& amount, const bool& beforeAfter = true) {
+    string lineString = chainText(" ", GetConsoleSize()[1]);
+
+    if (beforeAfter) {
+        cout << format("\u001b[{}A", amount);
+    }
+
+    for (unsigned int i = 0; i < amount; ++i) {
+        cout << lineString << endl;
+    }
+
+    if (!beforeAfter) {
+        cout << format("\u001b[{}B", amount);
+    }
+
+    cout.flush();
 }
+
+vector<string> splitText(const string& text, const char& separator) {
+    if (text.empty()) {
+        return {""};
+    }
+    vector<string> result;
+    string buffer;
+    for (const char& character : text) {
+        if ( character == separator) {
+            if (!buffer.empty()) {
+                result.push_back(buffer);
+                buffer.clear();
+            }
+        }
+        else {
+            buffer += character;
+        }
+    }
+    if (!buffer.empty()) {
+        result.push_back(buffer);
+    }
+    return result;
+}
+
+string toLowerString(const string& str) {
+    string result;
+    for (const char& character : str) {
+        result += tolower(character);
+    }
+    return result;
+}
+
+// *************** end of helper functions ***************
 
 void BuildGrid() {
     DataLayer row;
@@ -105,7 +186,7 @@ void render(const Position& cameraPosition = position, const Size& camera = scre
     buffer += format("\033[{}A", consoleSize[0]);
 
     static int OldConsoleWdith = 0;
-    string toCenter;
+    static string toCenter;
 
     //generating white space to center the image
     if (OldConsoleWdith != consoleSize[1]) {
@@ -149,9 +230,90 @@ void render(const Position& cameraPosition = position, const Size& camera = scre
     gridMap = globalGridMap;
 }
 
+void move(const Vector& direction, const int& tiles) {
+    for (int i = 0; i < tiles; ++i) {
+        Position newPosition = {position[0] + direction[0], position[1] + direction[1]};
+        if (isInBounds(newPosition)) {
+            if (isTraversable(newPosition)) {
+                position = newPosition;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            break;
+        }
+
+        render();
+
+        if (i < tiles - 1) {
+            Sleep(500);
+        }
+    }
+}
+
+Event commandHandler() {
+    Event event;
+    event["id"] = 0;
+
+    // cleanup
+    clearLines(3, false);
+
+    // input handling
+    cout << "\t $ ";
+    string input;
+    getline(cin, input);
+    vector<string> command = splitText(toLowerString(input), ' ');
+
+    // command and event handling
+    if (command[0] == "move") {
+        if (command.size() >= 2) {
+            if (possibleMovementTranslation.find(command[1]) != possibleMovementTranslation.end()) {
+                event["move"] = directionText[command[1]];
+                event["id"] = 1;
+            }
+        }
+        if (command.size() >= 3) {
+            try {
+                event["tiles"] = stoi(command[2]);
+            }
+            catch (...) {
+                event["tiles"] = 1;
+            }
+        }
+        else {
+            event["tiles"] = 1;
+        }
+    }
+    else if (command[0] == "clear") {
+        system("CLS");
+    }
+
+    clearLines(3, true);
+
+    return event;
+}
+
 int main() {
+    //initializing settings
     system("");
     BuildGrid();
-    render();
+
+    //event loop
+    while (true) {
+        render();
+
+        Event event = commandHandler();
+        int eventId = get<int>(event["id"]);
+
+        // event hande
+        if (eventId == 0) { // get<Type> -> specificaly for variant, used for getting a specific value out of it.
+            continue; // just for clarity...
+        }
+        else if (eventId == 1) {
+            move(CompassDirection[get<char>(event["move"])], get<int>(event["tiles"]));
+        }
+    }
     return 0;
 }
